2de3541f303abe69b85778581edaf0b5c530246b
Mike Thomas <mike.thomas2402@gmail.com>
2021-12-21 01:06:22 +0100

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script is not an actual install script by definition.
+# It is used to restart services after the git pull.
+
+# Restart Klipper
+sudo systemctl restart klipper.service
+
+# restart moonraker
+sudo systemctl restart moonraker.service
+
+# restart klipperscreen
+sudo systemctl restart klipperscreen.service
